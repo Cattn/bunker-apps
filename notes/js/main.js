6eb09a670d3fa209e40701fdf513e4503b7cad7f
Logan <thecrazykid2121@gmail.com>
2022-06-07 12:56:06 -0400

    push updates

---
+++ notes/js/main.js
@@ -0,0 +1,12 @@
+import NotesView from "./NotesView.js";
+
+
+const app = document.getElementById("app");
+const view = new NotesView(app, {
+    onNoteAdd() {
+        console.log("adding a new note");
+
+    }
+    
+    
+});
